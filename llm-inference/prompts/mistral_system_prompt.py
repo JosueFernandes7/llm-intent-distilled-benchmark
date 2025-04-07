b483c2e6c308a229efe8461cadecff395eae1924
Josué Fernandes <josue7mf@hotmail.com>
2025-04-07 15:55:03 -0300

    feat: add new dataset CLINC150

--- llm-inference/prompts/mistral_system_prompt.py
+++ llm-inference/prompts/mistral_system_prompt.py
@@ -2,10 +2,16 @@ MISTRAL_SYSTEM = """
 Você é um classificador de intenções.  
 Receberá frases de usuários e deverá classificá-las como uma das seguintes intenções:
 
-"saudacao",
-"solicitar_informacoes_produto",
-"falar_com_atendente_humano"
+{intencoes}
 
 Responda apenas com o nome da intenção mais adequada, sem explicações.
+
+Exemplos:
+{exemplo}
+
+Agora classifique a intenção da frase abaixo:
+
+Frase: "{nova_frase}"  
+Intenção:
 
 """
